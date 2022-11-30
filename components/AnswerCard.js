2e0d4569502665dfaa6d9a23a8141ac6da39ea06
Maria Litwa <contact@marialitwa.com>
2022-11-30 11:41:20 +0100

    Add FavIcon/Button to AnswerCard component

--- components/AnswerCard.js
+++ components/AnswerCard.js
@@ -3,8 +3,9 @@ import styled from "styled-components";
 
 export default function AnswerCard({ questionText, answerText }) {
   return (
-    <>
+    <StyledCard>
       <StyledQuestion>{questionText}</StyledQuestion>
+
       {answerText.length === 0 ? (
         <StyledPlaceholder>Kein Eintrag</StyledPlaceholder>
       ) : (
@@ -12,13 +13,45 @@ export default function AnswerCard({ questionText, answerText }) {
           <StyledAnswer>{morningAnswer.text}</StyledAnswer>
         ))
       )}
-    </>
+      <FavIcon
+        type={"button"}
+        variant={"favorite"}
+        onClick={() => {
+          toggleFavorite(answerText.id);
+        }}
+      >
+        {answerText.isFavorite ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="30px"
+            viewBox="0 0 24 24"
+            width="30px"
+            fill="#4d5b5b"
+          >
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="30px"
+            viewBox="0 0 24 24"
+            width="30px"
+            fill="#4d5b5b"
+          >
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path d="M16.5 3c-1.74 0-3.41.81-4.5 2.09C10.91 3.81 9.24 3 7.5 3 4.42 3 2 5.42 2 8.5c0 3.78 3.4 6.86 8.55 11.54L12 21.35l1.45-1.32C18.6 15.36 22 12.28 22 8.5 22 5.42 19.58 3 16.5 3zm-4.4 15.55l-.1.1-.1-.1C7.14 14.24 4 11.39 4 8.5 4 6.5 5.5 5 7.5 5c1.54 0 3.04.99 3.57 2.36h1.87C13.46 5.99 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5 0 2.89-3.14 5.74-7.9 10.05z" />
+          </svg>
+        )}
+      </FavIcon>
+    </StyledCard>
   );
 }
 
 const StyledQuestion = styled.p`
   display: flex;
   justify-content: center;
+
   margin-bottom: -10px;
   font-size: 0.8rem;
   color: #757474;
@@ -29,7 +62,7 @@ const StyledAnswer = styled.p`
   display: flex;
   justify-content: center;
   padding: 5px 40px;
-  margin-bottom: 60px;
+  margin-bottom: 10px;
   font-size: 1rem;
   text-align: center;
   font-weight: 300;
@@ -48,4 +81,16 @@ const StyledPlaceholder = styled.p`
   line-height: 140%;
   max-width: 100vw;
   font-style: italic;
+`;
+
+const StyledCard = styled.p`
+  _border: 1px solid hotpink;
+`;
+
+const FavIcon = styled.button`
+  display: flex;
+  justify-content: center;
+  border: none;
+  margin: auto;
+  margin-bottom: 4rem;
 `;
